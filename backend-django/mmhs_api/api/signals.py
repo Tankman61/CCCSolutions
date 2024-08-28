c0ab5cf4b417187648256b5315700a594a8c01dc
William Yang <willi64645@gmail.com>
2024-08-28 11:44:47 -0400

    initial commit + setup

---
+++ backend-django/mmhs_api/api/signals.py
@@ -0,0 +1,23 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
+from .models import UserProfile, Post
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+# Signal to save the UserProfile when the User is saved
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+# Signal to update the UserProfile's contributions when a Post is saved
+@receiver(post_save, sender=Post)
+def update_user_contributions(sender, instance, **kwargs):
+    profile = instance.author.profile
+    profile.contributions += 1
+    profile.update_username_color()
+    profile.save()
