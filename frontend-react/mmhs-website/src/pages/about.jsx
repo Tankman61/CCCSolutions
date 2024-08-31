569e27f77e669ac3ac0084783e8061981c4f10c5
William Yang <willi64645@gmail.com>
2024-08-30 20:29:31 -0400

    fixed names to contributors

--- frontend-react/mmhs-website/src/pages/about.jsx
+++ frontend-react/mmhs-website/src/pages/about.jsx
@@ -76,14 +76,16 @@ const About = () => {
         <div className="max-w-6xl mx-auto">
           <h2 className="text-4xl font-bold text-center mb-16">Contributors</h2>
           <p className="text-lg text-center mb-10">
-            We are grateful for the contributions of the following individuals who have helped build and maintain this platform:
+            We are grateful for the contributions of the following individuals who have helped contribute solutions to this website
           </p>
           <ul className="text-center space-y-4">
-            <li><strong>AJ</strong> - Amlesh Jayakumar of Waterloo Collegiate Institute (2008 S5: 2009 S4, S5: 2012 S4, 2014 S4)</li>
+            <li><strong>AJ</strong> - Amlesh Jayakumar of Waterloo Collegiate Institute (2008 S5: 2009 S4, S5: 2012 S4,
+              2014 S4)
+            </li>
             <li><strong>AL</strong> - Anton Likhtarov of Burnaby North S.S. (2005 S5)</li>
             <li><strong>AS</strong> - Ahmed Sabie of Glenforest Secondary School (2000 J5/S3, 1996 P5)</li>
             <li><strong>AT</strong> - Alex Tung of Hong Kong (2014 S4)</li>
-            <li><strong>AV</strong> - Aaron Voelker of Bell High School. (2007 S3: 2000 S4: 1999 P5)</li>
+            <li><strong>AV</strong> - Aaron Voelker of Bell High School (2007 S3: 2000 S4: 1999 P5)</li>
             <li><strong>BB</strong> - Brian Bi of Woburn Collegiate Institute (2009 S5)</li>
             <li><strong>CL</strong> - Calvin Liu of Glenforest Secondary School (2003 S4: 2014 S4, 2015 S4)</li>
             <li><strong>DC</strong> - Daniel Cressman of Westdale Secondary School (2011 J5: 2012 S1: 2012 S4)</li>
@@ -97,22 +99,27 @@ const About = () => {
             <li><strong>JJ2</strong> - Jacob Jackson of University of Toronto Schools (2014 S5)</li>
             <li><strong>KK</strong> - Kunal Khamar of Middlefield Collegiate Institute (2013 S3)</li>
             <li><strong>KL2</strong> - Kevin Luo of Eric Hamber Secondary School (2012 S5, 2012 S4)</li>
-            <li><strong>KL</strong> - Konstantin Lopyrev of the University of Waterloo. (2008 S3, S4, S5: 2007 J5, S5)</li>
-            <li><strong>ML</strong> - Matthew Lai of Steveston-London Secondary School. (2008 S5: 2007 S4)</li>
+            <li><strong>KL</strong> - Konstantin Lopyrev of the University of Waterloo (2008 S3, S4, S5: 2007 J5, S5)
+            </li>
+            <li><strong>ML</strong> - Matthew Lai of Steveston-London Secondary School (2008 S5: 2007 S4)</li>
             <li><strong>ML2</strong> - Matthew Lee of Galt Collegiate Institute (2015 S3)</li>
             <li><strong>NB</strong> - Nenad Bauk of Richview Collegiate Institute (2013 S2)</li>
             <li><strong>NV</strong> - Nazar Viznytsya (2016-2019)</li>
             <li><strong>PS</strong> - Peter Sun of St. Francis Xavier S.S. (2010 J5)</li>
-            <li><strong>RC</strong> - Robin Cheng of Pinetree Secondary (2005: S5)</li>
+            <li><strong>RC</strong> - Robin Cheng of Pinetree Secondary (2005 S5)</li>
             <li><strong>RH</strong> - Rich Hong of St. George's School, Vancouver (2006 S5)</li>
             <li><strong>RP</strong> - Richard Peng of Vaughan Road Academy (2004 S4: 2001 S4: 2000 S5)</li>
-            <li><strong>SH</strong> - Sean Henderson of the University of Toronto (2005 J5)</li>
-            <li><strong>TH</strong> - Tim Hu of University of Toronto (2010 S5)</li>
-            <li><strong>TK</strong> - Tamer Khater of Oakville Trafalgar High School (2007 S4)</li>
-            <li><strong>TM</strong> - Taufik Mohd of Danforth Collegiate and Technical Institute (2011 S5)</li>
-            <li><strong>WD</strong> - William Do of St. Michael's College School (2012 S4)</li>
-            <li><strong>ZH</strong> - Zeyu Huang of Yates Collegiate Institute (2013 S5)</li>
-            <li><strong>ZY</strong> - Zhanqi Yang of Humberside Collegiate Institute (2013 S5)</li>
+            <li><strong>SH</strong> - Sean Henderson of the University of Toronto (2005 S5)</li>
+            <li><strong>SF</strong> - Sumudu Fernando of the University of Alberta (2011 J5, 2011 S5, 2010 S3)</li>
+            <li><strong>VL</strong> - Victor Liu of Dr. Norman Bethune C.I. (2010 S5)</li>
+            <li><strong>VW</strong> - Victor Wang of Tecumseh Elementary (2013 S2)</li>
+            <li><strong>VS</strong> - Vassili Skarine of Northview Heights S.S. (2003 S4, S5: 2002 S4)</li>
+            <li><strong>VSi</strong> - Vincent Siao of St George's School (S010 S3)</li>
+            <li><strong>WZ</strong> - Weiwei Zhong of Glenforest Secondary School (2015 S3)</li>
+            <li><strong>WC</strong> - Wonjohn Choi of St. Francis Xavier S.S. (2010 J5)</li>
+            <li><strong>WHL</strong> - Wen-Hao Lue of Bayview Glen Private School (2007 J5: 2009 S4)</li>
+            <li><strong>YL</strong> - Yikuan Li of Don Mills Collegiate Institute (2014 S4)</li>
+            <li><strong>ZZ</strong> - Zihao Zhang of Vincent Massey Secondary School (2014 S4)</li>
             {/* Add more contributors as needed */}
           </ul>
         </div>
