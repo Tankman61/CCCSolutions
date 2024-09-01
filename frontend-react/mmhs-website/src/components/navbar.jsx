50e21c79726d79be0255ea63bf61e2bbd62f36ae
William Yang <willi64645@gmail.com>
2024-09-01 12:00:21 -0400

    adjusted sizing of logo

--- frontend-react/mmhs-website/src/components/navbar.jsx
+++ frontend-react/mmhs-website/src/components/navbar.jsx
@@ -6,8 +6,11 @@ const Navbar = () => {
     <nav className="bg-white p-4 shadow-lg">
       <div className="container mx-auto flex justify-between items-center">
         <div className="flex items-center">
-          <img src="https://cdn.discordapp.com/attachments/1174486466961866793/1279184503281287339/school_logo.png?ex=66d384e7&is=66d23367&hm=143e20e8b175b53c598e2781883b30c9312bca10a172eb4323acf115af770504&" alt="MMHS Logo" className="h-10 w-auto mr-3" />
-          <span className="text-2xl font-semibold">MMHS</span>
+          <img
+              src="https://cdn.discordapp.com/attachments/1174486466961866793/1279184503281287339/school_logo.png?ex=66d384e7&is=66d23367&hm=143e20e8b175b53c598e2781883b30c9312bca10a172eb4323acf115af770504&"
+              alt="MMHS Logo"
+              className="h-[48px] w-auto mr-3"
+          /> <span className="text-2xl font-semibold">Milliken Mills High School</span>
         </div>
         <ul className="flex space-x-8 text-lg">
           <li><Link to="/" className="hover:text-blue-400 transition-colors duration-300">Home</Link></li>
