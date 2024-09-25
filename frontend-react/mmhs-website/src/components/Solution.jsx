d26e05efdd3209aad457a8b35395d4520ab97d0f
William Yang <willi64645@gmail.com>
2024-09-24 21:24:58 -0400

    changed color for preview

--- frontend-react/mmhs-website/src/components/Solution.jsx
+++ frontend-react/mmhs-website/src/components/Solution.jsx
@@ -90,90 +90,90 @@ const Problem = ({ contestYear, problemCode }) => {
   const keywords = `CCC ${actualProblemCode} Solution, CCC ${problemCode}`;
 
   return (
-    <div className="bg-gray-200 min-h-screen p-8">
-      {/*Keywords for SEO + meta tags*/}
-      <Helmet>
-        <title>Solution: CCC {contestYear} {actualProblemCode.toUpperCase()}</title>
-        <meta name="keywords" content={keywords}/>
-        <meta property="og:title" content={`Solution: CCC ${contestYear} ${actualProblemCode.toUpperCase()}`}/>
-        <meta property="og:description"
-              content="The most comprehensive solution repository for the Canadian Computing Competition, with solutions to the CCC from 1996 to present."/>
-        <meta property="og:url" content={window.location.href}/>
-        <meta name="theme-color" content="#1e3a8a"/>
-      </Helmet>
-      <div className="w-full p-8 rounded-lg bg-white shadow-md">
-        <h2 className="text-2xl font-semibold text-black mb-4">
-          {`CCC ${contestYear} ${problemCode.toUpperCase()}`}
-        </h2>
+      <div className="bg-gray-200 min-h-screen p-8">
+        {/*Keywords for SEO + meta tags*/}
+        <Helmet>
+          <title>Solution: CCC {contestYear} {actualProblemCode.toUpperCase()}</title>
+          <meta name="keywords" content={keywords}/>
+          <meta property="og:title" content={`Solution: CCC ${contestYear} ${actualProblemCode.toUpperCase()}`}/>
+          <meta property="og:description"
+                content="The most comprehensive solution repository for the Canadian Computing Competition, with solutions to the CCC from 1996 to present."/>
+          <meta property="og:url" content={window.location.href}/>
+          <meta name="theme-color" content="#1e3a8a"/>
+        </Helmet>
+        <div className="w-full p-8 rounded-lg bg-white shadow-md">
+          <h2 className="text-2xl font-semibold text-black mb-4">
+            {`CCC ${contestYear} ${problemCode.toUpperCase()}`}
+          </h2>
 
-        {actualProblemCode !== problemCode && (
-            <p className="text-sm text-gray-600 mb-4">
-              Note: Showing solution for {actualProblemCode.toUpperCase()} as it's
-            equivalent to {problemCode.toUpperCase()}.
-          </p>
-        )}
+          {actualProblemCode !== problemCode && (
+              <p className="text-sm text-gray-600 mb-4">
+                Note: Showing solution for {actualProblemCode.toUpperCase()} as it's
+                equivalent to {problemCode.toUpperCase()}.
+              </p>
+          )}
 
-        <div className="mb-6">
-          <h3 className="text-lg font-medium text-black mb-4">Test Cases:</h3>
-          <div className="bg-gray-100 p-4 rounded-lg w-1/2 mx-auto">
-            <div className="flex space-x-2 mb-4 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-400">
-              {Array.from({ length: 30 }, (_, idx) => (
-                <button
-                  key={idx}
-                  onClick={() => handleTabClick(idx)}
-                  className={`px-4 py-2 flex-shrink-0 ${
-                    activeTab === idx ? "bg-blue-800 text-white" : "bg-gray-200 text-black"
-                  } rounded-lg`}
-                >
-                  Case {idx + 1}
-                </button>
-              ))}
-            </div>
+          <div className="mb-6">
+            <h3 className="text-lg font-medium text-black mb-4">Test Cases:</h3>
+            <div className="bg-gray-100 p-4 rounded-lg w-1/2 mx-auto">
+              <div className="flex space-x-2 mb-4 overflow-x-auto scrollbar-thin scrollbar-thumb-gray-400">
+                {Array.from({length: 30}, (_, idx) => (
+                    <button
+                        key={idx}
+                        onClick={() => handleTabClick(idx)}
+                        className={`px-4 py-2 flex-shrink-0 ${
+                            activeTab === idx ? "bg-blue-800 text-white" : "bg-gray-200 text-black"
+                        } rounded-lg`}
+                    >
+                      Case {idx + 1}
+                    </button>
+                ))}
+              </div>
 
-            <div className="p-4 bg-white rounded-lg">
-              {isValidTestCase(testCaseData.input) && isValidTestCase(testCaseData.output) ? (
-                <>
-                  <p className="text-black mb-2">
-                    <strong>Input:</strong>
-                  </p>
-                  <textarea
-                    className="w-full h-20 p-2 mb-4 bg-gray-100 text-black border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    readOnly
-                    value={testCaseData.input}
-                  />
-                  <p className="text-black mb-2">
-                    <strong>Output:</strong>
-                  </p>
-                  <textarea
-                    className="w-full h-20 p-2 bg-gray-100 text-black border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    readOnly
-                    value={testCaseData.output}
-                  />
-                </>
-              ) : (
-                <p className="text-red-600 font-bold text-center">
-                  Test case irretrievable or unavailable
-                </p>
-              )}
+              <div className="p-4 bg-white rounded-lg">
+                {isValidTestCase(testCaseData.input) && isValidTestCase(testCaseData.output) ? (
+                    <>
+                      <p className="text-black mb-2">
+                        <strong>Input:</strong>
+                      </p>
+                      <textarea
+                          className="w-full h-20 p-2 mb-4 bg-gray-100 text-black border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                          readOnly
+                          value={testCaseData.input}
+                      />
+                      <p className="text-black mb-2">
+                        <strong>Output:</strong>
+                      </p>
+                      <textarea
+                          className="w-full h-20 p-2 bg-gray-100 text-black border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"
+                          readOnly
+                          value={testCaseData.output}
+                      />
+                    </>
+                ) : (
+                    <p className="text-red-600 font-bold text-center">
+                      Test case irretrievable or unavailable
+                    </p>
+                )}
+              </div>
             </div>
           </div>
-        </div>
 
-        <div className="mb-6">
-          <h3 className="text-lg font-medium text-black mb-2">Solutions:</h3>
-          {solutions.map((solution, idx) => (
-            <div key={idx} className="mb-4">
-              <h4 className="text-md font-medium text-black mb-1">
-                Solution {idx + 1}:
-              </h4>
-              <SyntaxHighlighter language="cpp" style={solarizedlight} showLineNumbers>
-                {solution}
-              </SyntaxHighlighter>
-            </div>
-          ))}
+          <div className="mb-6">
+            <h3 className="text-lg font-medium text-black mb-2">Solutions:</h3>
+            {solutions.map((solution, idx) => (
+                <div key={idx} className="mb-4">
+                  <h4 className="text-md font-medium text-black mb-1">
+                    Solution {idx + 1}:
+                  </h4>
+                  <SyntaxHighlighter language="cpp" style={solarizedlight} showLineNumbers>
+                    {solution}
+                  </SyntaxHighlighter>
+                </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
   );
 };
 
