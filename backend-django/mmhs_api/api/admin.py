0cadeca0b7a21f01a962fabe9205c17310f3a977
AStrangePotato <danielzhang.936@gmail.com>
2024-09-26 08:14:16 -0700

    file structure change

--- backend-django/mmhs_api/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Forum, Thread, Post, UserProfile
-
-admin.site.register(Forum)
-admin.site.register(Thread)
-admin.site.register(Post)
-admin.site.register(UserProfile)
