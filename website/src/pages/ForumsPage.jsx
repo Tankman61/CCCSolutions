73b8853bc1e3627b1be3177f14dc7856d88efa1d
William Yang <willi64645@gmail.com>
2024-11-21 13:29:19 -0500

    fixes padding issue with forums page

--- website/src/pages/ForumsPage.jsx
+++ website/src/pages/ForumsPage.jsx
@@ -58,7 +58,7 @@ export default function ForumPage() {
   return (
     <div className="font-poppins">
       <title>Forums</title>
-      <div className="bg-gradient-to-r from-blue-800 to-indigo-900 text-white p-12">
+      <div className="bg-gradient-to-r from-blue-800 to-indigo-900 text-white py-16 px-4">
         <div className="container mx-auto text-center">
           <h1 className="text-5xl font-bold mb-4">Forums</h1>
           <p className="text-xl md:text-2xl max-w-2xl mx-auto mb-5">
